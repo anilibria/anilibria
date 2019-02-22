73b5df2047bb39e563a3220c37fea2c607ad7305
Aleksey Orekhovsky <aaa4xu@gmail.com>
2019-02-22 21:49:19 +0300

    Поддержка переменных окружения
    Возможность конфигурирования через переменные окружения требуется для более удобной упаковки в Docker.

--- private/config.php
+++ private/config.php
@@ -3,38 +3,38 @@ $conf['start'] = microtime(true);
 
 $conf['memcache']	= ['/tmp/memcached.socket', 0];
 
-$conf['mysql_host'] = 'localhost';
-$conf['mysql_user'] = 'anilibria';
-$conf['mysql_pass'] = 'anilibria';
-$conf['mysql_base'] = 'anilibria';
+$conf['mysql_host'] = getenv('MYSQL_HOST', true) ?: 'localhost';
+$conf['mysql_user'] = getenv('MYSQL_USERNAME', true) ?: 'anilibria';
+$conf['mysql_pass'] = getenv('MYSQL_PASSWORD', true) ?: 'anilibria';
+$conf['mysql_base'] = getenv('MYSQL_DATABASE', true) ?: 'anilibria';
 
-$conf['email'] = 'poiuty@poiuty.com';
-$conf['email_from'] = 'Test anilibria';
+$conf['email'] = getenv('EMAIL_FROM', true) ?: 'poiuty@poiuty.com';
+$conf['email_from'] = getenv('EMAIL_NAME', true) ?: 'Test anilibria';
 
 // v3
-$conf['recaptcha_secret'] = 'secret';
-$conf['recaptcha_public'] = 'public';
+$conf['recaptcha_secret'] = getenv('RECAPTCHA2_SECRET', true) ?: 'secret';
+$conf['recaptcha_public'] = getenv('RECAPTCHA2_PUBLIC', true) ?: 'public';
 
 // v2
-$conf['recaptcha2_secret'] = 'secret';
-$conf['recaptcha2_public'] = 'public';
+$conf['recaptcha2_secret'] = getenv('RECAPTCHA3_SECRET', true) ?: 'secret';
+$conf['recaptcha2_public'] = getenv('RECAPTCHA3_PUBLIC', true) ?: 'public';
 
 $conf['hash_len'] = 64;
 $conf['hash_algo'] = 'sha256';
 
-$conf['torrent_secret'] = 'secret';
-$conf['torrent_announce'] = 'http://tt.anilibria.tv:2710/announce';
+$conf['torrent_secret'] = getenv('TORRENT_SECRET', true) ?: 'secret';
+$conf['torrent_announce'] = getenv('TORRENT_ANNOUNCE', true) ?: 'http://tt.anilibria.tv:2710/announce';
 
-$conf['sphinx_host'] = '127.0.0.1';
-$conf['sphinx_port'] = '9306';
+$conf['sphinx_host'] = getenv('SPHINX_HOST', true) ?: '127.0.0.1';
+$conf['sphinx_port'] = getenv('SPHINX_PORT', true) ?: '9306';
 
-$conf['stat_url'] = 'https://ws.poiuty.com/ws/';
-$conf['stat_secret'] = 'secret';
+$conf['stat_url'] = getenv('STAT_URL', true) ?: 'https://ws.poiuty.com/ws/';
+$conf['stat_secret'] = getenv('STAT_SECRET', true) ?: 'secret';
 
-$conf['nginx_domain'] = 'https://x.anilibria.tv';
-$conf['nginx_secret'] = 'secret';
+$conf['nginx_domain'] = getenv('NGINX_BASE', true) ?: 'https://x.anilibria.tv';
+$conf['nginx_secret'] = getenv('NGINX_SECRET', true) ?: 'secret';
 
-$conf['youtube_secret'] = 'secret';
+$conf['youtube_secret'] = getenv('YOUTUBE_SECRET', true) ?: 'secret';
 
-$conf['push_all'] = 'secret';
+$conf['push_all'] = getenv('PUSHALL_SECRET', true) ?: 'secret';
 $conf['push_sanasol'] = 'secret';
